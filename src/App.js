7f74d94dcfb5dafcb459343f6e63bd522dc697b1
kimminkyu <kmk50009@naver.com>
2022-02-10 14:26:28 +0900

    Add Transition Event

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
-    showBlock: false
+    showBlock: false,
   };
 
   showModal = () => {
@@ -27,35 +27,39 @@ class App extends Component {
         <button
           className="Button"
           onClick={() =>
-            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
         >
           Toggle
         </button>
         <br />
-        <Transition 
-          in={this.state.showBlock} 
+        <Transition
+          in={this.state.showBlock}
           timeout={1000}
           mountOnEnter
-          unmountOnExit>
-          {state => (
+          unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
+        >
+          {(state) => (
             <div
               style={{
                 backgroundColor: "red",
                 width: 100,
                 height: 100,
                 margin: "auto",
-                transition: 'opacity 1s ease-out',
-                opacity: state === 'exiting' ? 0 : 1
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
               }}
             />
           )}
         </Transition>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
